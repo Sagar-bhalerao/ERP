23b72226d5874b7fe32e6cf8858dab6e1509da79
aakash.dighe <akashdighe2205@gmail.com>
2024-06-27 11:18:07 +0530

    Second commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,6 @@ import Index from './Index'
 
 function App() {
   console.log("Hello From akash");
-  console.log("Hello From akash");
-
-  console.log("Hello From akash");
-
-
 
   const { isAuthenticated, user } = useSelector((state: any) => state.auth);
   console.log(isAuthenticated, user);
