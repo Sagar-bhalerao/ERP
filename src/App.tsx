76b9f5affe9c83030cb45220268aa9c35e95500a
aakash.dighe <akashdighe2205@gmail.com>
2024-06-27 11:37:36 +0530

    Try commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,11 @@ import './App.css'
 import Index from './Index'
 
 function App() {
-  console.log("Hello From akash");
+  console.log("Hello From akash1");
+  console.log("Hello From akash2");
+  console.log("Hello From akash3");
+  console.log("Hello From akash4");
+
 
   const { isAuthenticated, user } = useSelector((state: any) => state.auth);
   console.log(isAuthenticated, user);
